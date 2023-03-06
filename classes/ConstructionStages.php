863b25bcd0ce5e2462af2aaa9d42072732e3a4ae
Nader Awadala <naderawadala@gmail.com>
2023-03-06 17:52:00 +0200

    wrap up documentation and readme

--- classes/ConstructionStages.php
+++ classes/ConstructionStages.php
@@ -1,15 +1,32 @@
 <?php
 
+/**
+ * Summary of ConstructionStages
+ * Class that contains all the functions for basic CRUD functionality related to construction_stages
+ * table in the database.
+ */
 class ConstructionStages
 {
 	private $db;
+	/**
+	 * Summary of validDateRegex
+	 * Regex that checks if a date is valid according to the ISO 8601 standard
+	 */
 	private $validDateRegex = '/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/';
+	/**
+	 * Summary of validColorCodeRegex
+	 * Regex that checks if a color code is valid according to hex color codes
+	 */
 	private $validColorCodeRegex = '/#([a-fA-F0-9]{3}){1,2}\b/';
 	public function __construct()
 	{
 		$this->db = Api::getDb();
 	}
 
+	/**
+	 * Summary of getAll
+	 * Gets all items from the construction_stages table and returns them.
+	 */
 	public function getAll()
 	{
 		$stmt = $this->db->prepare("
@@ -29,6 +46,11 @@ class ConstructionStages
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	/**
+	 * Summary of getSingle
+	 * @param $id is the id of the requested construction_stage
+	 * Gets one item from the construction_stages table by id and returns it.
+	 */
 	public function getSingle($id)
 	{
 		$stmt = $this->db->prepare("
@@ -49,6 +71,13 @@ class ConstructionStages
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	/**
+	 * Summary of post
+	 * Creates a new construction_stage and adds it to the table, it either returns an array
+	 * of errors when validation fails or returns the newly created item if successfully created.
+	 * @param ConstructionStagesCreate $data is the body for creating a new item
+	 * @return array
+	 */
 	public function post(ConstructionStagesCreate $data)
 	{
 		$errors = array();
@@ -116,6 +145,13 @@ class ConstructionStages
 		return $errors;
 	}
 
+	/**
+	 * Summary of patch
+	 * Edits an already existing construction_stage, it either returns an array
+	 * of errors when validation fails or returns the newly created item if successfully created.
+	 * @param ConstructionStagesEdit $data
+	 * @return array
+	 */
 	public function patch(ConstructionStagesEdit $data)
 	{
 		$errors = array();
@@ -187,11 +223,18 @@ class ConstructionStages
 				'externalId' => $externalId,
 				'status' => $status,
 			]);
-			return $data;
+			return $this->getSingle($id);
 		}
 		return $errors;
 	}
 
+	/**
+	 * Summary of delete
+	 * The name says delete but it actually does not delete an element from the construction_stages 
+	 * table, but simply sets the variable status as 'DELETED'.
+	 * @param mixed $id is the id of the element
+	 * @return array
+	 */
 	public function delete($id)
 	{
 		$stmt = $this->db->prepare("
@@ -207,11 +250,21 @@ class ConstructionStages
 		]);
 		return $this->getSingle($id);
 	}
+	/**
+	 * Summary of calculateDuration
+	 * A helper function that is used to calculate the duration row for the construction_stages table
+	 * that returns null if the end date does not exist or the start date is after the end date.
+	 * Different intervals are returned depending upon the value of the durationUnit variable.
+	 * @param mixed $startDate 
+	 * @param mixed $endDate 
+	 * @param mixed $durationUnit
+	 * @return int|null
+	 */
 	private function calculateDuration($startDate, $endDate, $durationUnit)
 	{
 		$end = new DateTime($endDate);
 		$start = new DateTime($startDate);
-		$interval = $end->diff($start);
+		$interval = $start->diff($end);
 
 		if ($endDate == null) {
 			return null;
@@ -226,7 +279,7 @@ class ConstructionStages
 			return $interval->h;
 		}
 		if ($durationUnit == "WEEKS") {
-			return $interval-> d / 7;
+			return $interval-> d / 7.0;
 		}
 	}
 }
